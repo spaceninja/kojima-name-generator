fb01dae03c2b3ce715f9827bf3897a77e24d09fb
Scott Vandehey <fake-scott@spaceninja.com>
2019-11-28 12:27:24 -0800

    add stylelint

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -100,18 +100,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+$green: #42b983;
+
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
-  color: #42b983;
+  color: $green;
 }
 </style>
